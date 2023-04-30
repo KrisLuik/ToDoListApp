500b75374af967967b4d0ea28eb3dfb01f91a6a2
Kris Tribbeck <kristribbeck@gmail.com>
2023-04-30 15:42:45 +0800

    Updated stack navigation - created separate stacks for Home, Categories, and Settings

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -4,16 +4,34 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from './screens/Home';
 import Details from './screens/Details'; // Updated import path
+import Categories from './screens/Categories';
+import Settings from './screens/Settings';
 
-const Stack = createStackNavigator();
+const HomeStack = createStackNavigator();
+const CategoriesStack = createStackNavigator();
+const SettingsStack = createStackNavigator();
 
-const MainStackNavigator = () => {
+const HomeStackNavigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name='Home' component={Home} />
-      <Stack.Screen name='Details' component={Details} />
-    </Stack.Navigator>
+    <HomeStack.Navigator>
+      <HomeStack.Screen name='Home' component={Home} options={{headerShown: false}}/>
+      <HomeStack.Screen name='Details' component={Details} />
+    </HomeStack.Navigator>
   );
-}
+};
+const CategoriesStackNavigator = () => {
+  return (
+    <CategoriesStack.Navigator>
+      <CategoriesStack.Screen name='Categories' component={Categories} options={{headerShown: false}}/>
+    </CategoriesStack.Navigator>
+  );
+};
+const SettingsStackNavigator = () => {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name='Settings' component={Settings} options={{headerShown: false}}/>
+    </SettingsStack.Navigator>
+  );
+};
 
-export { MainStackNavigator };
+export { HomeStackNavigator, CategoriesStackNavigator, SettingsStackNavigator };
--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,4 +1,4 @@
-import { MainStackNavigator} from './StackNavigator';
+import { HomeStackNavigator, CategoriesStackNavigator, SettingsStackNavigator } from './StackNavigator';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -11,45 +11,45 @@ import Settings from './screens/Settings';
 import Categories from './screens/Categories';
 
 const home = 'Home';
-const categories= 'Categories';
+const categories = 'Categories';
 const settings = 'Settings';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
-  return (
-    <NavigationContainer>
-        <Tab.Navigator
-            initialRouteName={'Home'}
-            screenOptions={({route}) => ({
-                tabBarIcon: ({focused, color, size}) => {
-                    let iconName;
-                    let routeName = route.name;
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                initialRouteName={'Home'}
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+                        let routeName = route.name;
 
-                    if (routeName == 'Home'){
-                        iconName = focused ? 'home' : 'home-outline';
-                    } else if (routeName == 'Categories'){
-                        iconName = focused ? 'list' : 'list-outline';
-                    } else if (routeName == 'Settings'){
-                        iconName = focused ? 'settings' : 'settings-outline';
-                    }
-                    return <IonIcons name={iconName} size={size} color={color} />
-                },
-            })}
-            tabBarOptions={{
-                activeTintColor: 'black',
-                inactiveTintColor: 'grey',
-                labelStyle: {paddingBottom: 10, fontSize: 10 },
-                style: {padding: 10, height: 70}
-            }}     
+                        if (routeName == 'Home') {
+                            iconName = focused ? 'home' : 'home-outline';
+                        } else if (routeName == 'Categories') {
+                            iconName = focused ? 'list' : 'list-outline';
+                        } else if (routeName == 'Settings') {
+                            iconName = focused ? 'settings' : 'settings-outline';
+                        }
+                        return <IonIcons name={iconName} size={size} color={color} />
+                    },
+                })}
+                tabBarOptions={{
+                    activeTintColor: 'black',
+                    inactiveTintColor: 'grey',
+                    labelStyle: { paddingBottom: 10, fontSize: 10 },
+                    style: { padding: 10, height: 70 }
+                }}
             // Need to leave tab.screen as Home to be able to see icons.
             >
-      <Tab.Screen name='Home' component={MainStackNavigator} />
-      <Tab.Screen name='Categories' component={Categories} />
-      <Tab.Screen name='Settings' component={Settings} />
-    </Tab.Navigator>
-    </NavigationContainer> 
-  );
+                <Tab.Screen name='Home' component={HomeStackNavigator} />
+                <Tab.Screen name='Categories' component={CategoriesStackNavigator} />
+                <Tab.Screen name='Settings' component={SettingsStackNavigator} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
 
 export default BottomTabNavigator;
